0cd573be09424bf09bb17fdfabb664a95009fefa
jdbeene72 <jdbeene@ualr.edu>
2021-05-26 17:31:36 +0000

    test

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -1,23 +1,54 @@
-/*$('.form-group').on('input','.prc',function(){
-  var totalSum = 0;
-  $('.form-group .prc').each(function) {
-    var inputVal = $(this).val();
-    if($.isNumeric(inputVal)) {
-      totalSum += parseFloat(inputVal);
-    }
-  });
-  $('#result').val(totalSum);
-}); */
+$(function(){
+      $(document).ready(function() {
+          $("#myModal #numguest").keyup(function(){
+              totalCraw = $("#myModal #numguest").val()* $("#myModal #crawqty").val();
+              $("#myModal #totalCraw").val(totalCraw);
+          });
+          
+          $("#myModal #numguest").keyup(function(){
+              totalSausage = $("#myModal #numguest").val()* .25;
+              $("#myModal #totalSausage").val(totalSausage);
+          });
+          
+          $("#myModal #numguest").keyup(function(){
+              totalPot = $("#myModal #numguest").val()* 1;
+              $("#myModal #totalPot").val(totalPot);
+          });
+          
+          $("#myModal #numguest").keyup(function(){
+              totalCorn = $("#myModal #numguest").val()* 1;
+              $("#myModal #totalCorn").val(totalCorn);
+          });
+          
+          $("#myModal #numguest").keyup(function(){
+              totalMush = $("#myModal #numguest").val()* 2;
+              $("#myModal #totalMush").val(totalMush);
+          });
+      });
+});
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function Calculate() {
-  var people = document.getElementById("numPeople").value;
-  var crawfish = 3;
-  var potato = 1;
-  var corn = 1.5;
-  var mushroom = 2;
-  var resultCraw = parseInt(people) * parseInt(crawfish);
-  var resultPotw = parseInt(people) * parseInt(potato);
-  var resultCorn = parseInt(people) * parseInt(corn);
-  var resultMush = parseInt(people) * parseInt(mushroom); 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
